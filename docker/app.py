94dad83fa0ae57042baacb8d48bf7ab42f97f4fe
Martha Obitube <74656858+chinelo-obitube@users.noreply.github.com>
2022-03-23 08:35:17 +0100

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
 
-    return "<h1>Welcome to SCA Cloud School Application</h1>"
+    return "<h1>Welcome to SCA Cloud School Application, this is my first assessment</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
